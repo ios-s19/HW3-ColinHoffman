4ba8e124f7f9b15c0fca8f74a4afaf35a88043d7
Colin Hoffman <c.hoffman2009@yahoo.com>
2019-03-19 19:13:19 -0500

    initial commit

---
+++ iOS-Spring-2019/iOS-Spring-2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-Spring-2019
+//
+//  Created by Colin Hoffman on 3/19/19.
+//  Copyright © 2019 University of Wisconsin-Parkside. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
